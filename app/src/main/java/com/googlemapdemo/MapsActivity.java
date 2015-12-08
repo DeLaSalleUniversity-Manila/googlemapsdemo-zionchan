9b561516c2c84059eace870c1fb39739ccd7460f
zionchan <zion_chan@dlsu.edu.ph>
2015-12-08 17:17:35 +0800

    yay

---
+++ app/src/main/java/com/googlemapdemo/MapsActivity.java
@@ -0,0 +1,35 @@
+package com.googlemapdemo;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng dlsu_manila = new LatLng(14.56489, 120.993851);
+        mMap.addMarker(new MarkerOptions().position(dlsu_manila).title("Marker in DLSU-Manila"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(dlsu_manila));
+    }
+}
